af0f48ab0131d6bd9fa342304f016d424aa5f9fb
Igor Sharoff <igor_se@mail.ru>
2017-05-30 13:55:42 +0300

    30.05.17

--- src/ru/geekbrains/chat/server/Controller.java
+++ src/ru/geekbrains/chat/server/Controller.java
@@ -2,6 +2,7 @@ package ru.geekbrains.chat.server;
 
 import javafx.application.Platform;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 import java.net.URL;
@@ -27,6 +28,12 @@ public class Controller implements Initializable {
     // по нажатию кнопки остановка сервера
     public void offline() {
         srv.stopServer();
+    }
+
+    public void clientsListClick(MouseEvent mouseEvent) {
+        if (mouseEvent.getClickCount() == 2) {
+
+        }
     }
 
 
